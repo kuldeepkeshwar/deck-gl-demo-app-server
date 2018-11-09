62d94fd8f7ca38e8eb8002dd78600205bf655861
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2018-11-10 00:37:50 +0530

    fetch files from github

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ const cors = require('cors')
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
+const request = require('request');
+const files=["earthquakes.csv","heatmap.csv","scatterplot.csv"];
+const remote="https://raw.githubusercontent.com/kuldeepkeshwar/deck-gl-demo-app-server/master/src/data/";
+
 app.use(cors());
+
+app.get('ping', (req, res) => {
+  res.end('pong!!');
+})
 app.get("/data/:filename", function(req, res,next) {
   const filename=req.params.filename;
   console.log(`starting streaming ${filename}`)  
-  const instream = fs.createReadStream(__dirname+
-    `/data/${filename}.csv`
-  );
+  const instream = fs.createReadStream(`/tmp/${filename}.csv`);
   instream.on("error", function(err) {
     next(err);
   });
@@ -35,3 +41,15 @@ app.use(function(err, req, res, next) {
   res.status(500).send("Something broke!");
 });
 app.listen(port, (...args) => console.log(`Example app listening on port ${port}!`,...args));
+
+files.forEach(file=>{
+  console.log(`fetching : ${remote}${file}`)
+  request.get(`${remote}${file}`, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        fs.writeFileSync("/tmp/"+file,body)
+        console.log("created file:",file);
+    }else{
+      console.log("error in building cache",error,response);
+    }
+});
+})
